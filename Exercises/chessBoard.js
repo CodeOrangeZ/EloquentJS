253038e88f3f597cc0859049dd7d9b37fa76026e
Zackery Rilling <rillingz@yahoo.com>
2017-10-23 19:00:34 -0700

    chessBoard exercise completed

---
+++ Exercises/chessBoard.js
@@ -0,0 +1,24 @@
+
+function chessBoard (size) {
+    var hash = '#', space = ' ', result = '';
+   
+    for (var i = 0; i < size; i++) {        
+
+        result += '\n'
+
+        for (var j = 0; j < size; j++) {
+        
+            if((i +j) % 2 == 0) {
+                result  += space
+            }
+            else {
+                result  += "#"
+            };
+        };
+
+    };
+
+    console.log(result);
+};
+
+chessBoard(8);
